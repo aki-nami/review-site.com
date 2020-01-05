4f9733d59766453ac1c7f19a6101639f7a31d6b8
akihirosako <akihiro@example.com>
2020-01-05 15:32:55 +0700

    [update]テンプレートエンジンを使用し表示部分を削除

--- list.php
+++ list.php
@@ -4,6 +4,9 @@ include('../lib/session.php');
 Session::loginCheck();
 define('COMMENTS_PER_PAGE', 5);
 
+include('MySmarty.php');
+$smarty = MySmarty::getSmarty();
+
 if (!empty($_GET['page'])) {
     $page = (int)$_GET['page'];
 } else {
@@ -19,41 +22,16 @@ if (!empty($_GET['page'])) {
 $db = new DB();
 $offset = COMMENTS_PER_PAGE * ($page - 1);
 $sql = "select * from contents limit {$offset}, 5";
-$box = $db->fetchAll($sql);
+$contents = $db->fetchAll($sql);
 
 $sql2 = ('select count(*) from contents');
-$box2 = $db->fetchColumn($sql2);
-$total_pages = ceil($box2 / COMMENTS_PER_PAGE);
+$contents_count = $db->fetchColumn($sql2);
+$total_pages = ceil($contents_count / COMMENTS_PER_PAGE);
+
+$smarty->assign('contents', $contents)
+    ->assign('total_pages', $total_pages)
+    ->display('templates/list.tpl');
+
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>一覧画面</title>
-    </head>
-    <body>
-    <a href="./index.php">TOPページはこちら</a>
-        <table border="3">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タイトル</th>
-                    <th>画像</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($box as $value) : ?>
-                <tr>
-                    <td><?php echo $value['content_id']; ?></td>
-                    <td><a href="post.php?id=<?php echo $value['content_id']; ?>"><?php echo $value['content_title']; ?></td>
-                    <td><img src="<?php echo '/upload_image/' . $value['content_file_name']; ?>"></td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-        <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-        <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-        <?php endfor; ?>
-    </body>
-</html>
+
